d1246afcb368ee9d756da3e75da3e97094b31c09
simon renger <simon@localhost.localdomain>
2019-08-15 23:19:18 +0200

    Added List<T> swap extension

--- MazeGenerator/Utils/ListExtensions.cs
+++ MazeGenerator/Utils/ListExtensions.cs
@@ -8,16 +8,16 @@ namespace Matze.Utils
 {
     public static class ListExtensions
     {
-        public static void Shuffle<T>(this IList<T> ts,ref Random rand)
+        public static void Shuffle<T>(this IList<T> ts, ref Random rand)
         {
             var count = ts.Count;
             var last = count - 1;
             for (var i = 0; i < last; ++i)
             {
-                    var r = rand.Next(i, count);
-                    var tmp = ts[i];
-                    ts[i] = ts[r];
-                    ts[r] = tmp;
+                var r = rand.Next(i, count);
+                var tmp = ts[i];
+                ts[i] = ts[r];
+                ts[r] = tmp;
             }
         }
         public static T Pop<T>(this IList<T> ts)
@@ -26,10 +26,19 @@ namespace Matze.Utils
             ts.RemoveAt(ts.Size());
             return pop;
         }
-
+        /**
+         * @brief Requests the Size of the List minus 1
+         */
         public static int Size<T>(this IList<T> ts)
         {
             return ts.Count - 1;
+        }
+        // Source https://stackoverflow.com/questions/2094239/swap-two-items-in-listt
+        public static void Swap<T>(IList<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
         }
     }
 }

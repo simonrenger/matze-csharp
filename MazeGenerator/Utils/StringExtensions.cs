afbe2750664bd5a05433c56dbdbb6ca7ef00a13f
simon renger <simon@localhost.localdomain>
2019-08-16 19:29:37 +0200

    added string extension for replaceAt index

---
+++ MazeGenerator/Utils/StringExtensions.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Matze.Utils
+{
+    public static class StringExtensions
+    {
+        public static string ReplaceAt(this string input, int index, char newChar)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            char[] chars = input.ToCharArray();
+            chars[index] = newChar;
+            return new string(chars);
+        }
+    }
+}
